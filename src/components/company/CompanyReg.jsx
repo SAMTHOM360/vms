0832becabf7e28690ffa40569c14f37022aa2a8c
HARSHITA77105 <21mca22@imit.ac.in>
2023-10-30 17:42:51 +0530

    this is demo

--- src/components/company/CompanyReg.jsx
+++ src/components/company/CompanyReg.jsx
@@ -1,3 +1,9 @@
+//hello somya how are you
+
+//whatsup
+
+//zimbra
+
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import {useNavigate} from 'react-router-dom';
--- src/components/company/CompanyTable.jsx
+++ src/components/company/CompanyTable.jsx
@@ -1,3 +1,6 @@
+
+//companytable git demo
+
 // import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
 // import { Link } from 'react-router-dom';
