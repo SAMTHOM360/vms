7c979b621125ad40a6f7f8891dc6b20477655bd9
HARSHITA77105 <21mca22@imit.ac.in>
2023-12-13 11:27:04 +0530

    done

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,7 +1,7 @@
 const Config = Object.freeze({
-  baseUrl: "https://192.168.12.54:8080/", //BISWAJIT
+  // baseUrl: "http://192.168.12.54:8080/", //BISWAJIT
   // baseUrl: "http://192.168.12.58:8080/", //SMRUTI
-  // baseUrl: "http://vms.nyggs.com/backend/",
+  baseUrl: "https://vms.nyggs.com/backend/",
 
     ownerSiteLink: "https://www.rapidsofttechnologies.com/",
 
